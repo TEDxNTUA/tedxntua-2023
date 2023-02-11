82f4ff39aa11e745bc982efde2ab7a8a34914139
dinospapargyriou <dinospapargyriou@gmail.com>
2023-02-11 17:42:40 +0200

    semi final partners

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Page from '../components/Page';
 import PageHead from '../components/PageHead';
-import { PartnerComp } from '../components/partnerComp';
+import {Partner} from "../components/Partner"
+import * as PartnersStyles from "../styles/partners.module.css"
 
 const pageTitle = 'Partners';
 
 const Partners = () => {
+const [open, setOpen] = React.useState(false);
+// const handleOpen = () => setOpen(true);
+// const handleClose = () => setOpen(false);
+console.log(open);
     return (
         <Page>
-            <div>
-                <PartnerComp/>
+            <div className={PartnersStyles.titles}>
+                <h2 className={PartnersStyles.title}>Knowledge Partner</h2>
+                <div className={PartnersStyles.section}>
+                    <Partner trigger={() => setOpen(true)}/>
+                    <Partner trigger={() => setOpen(true)}/>
+                    <Partner trigger={() => setOpen(true)}/>
+                    <Partner trigger={() => setOpen(true)}/>
+                    <Partner trigger={() => setOpen(true)}/>
+                    <Partner trigger={() => setOpen(true)}/>
+                </div>
+                
             </div>
         </Page>
     )
