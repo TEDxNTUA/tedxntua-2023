79bb8f8ad830605c0a4a55c1841f133635f81fcc
yiannisha <hadjiyiannis.3@gmail.com>
2023-02-13 18:25:58 +0200

    Fix production team name

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
         },
         {
             code: "PRODUCTION",
-            name: "VENUE_&_PRODUCTION",
+            name: "PRODUCTION",
         },
         {
             code: "FUNDRAISING",
