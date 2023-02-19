c1cb180fa1087e0cf81a2ae16de92e44f1c07df2
dinospapargyriou <dinospapargyriou@gmail.com>
2023-02-19 17:34:57 +0200

    partners 19/02

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -15,27 +15,27 @@ export const Partner = ({trigger}) => {
         <StaticImage className={partnerStyles.image} src="../images/example.jpg"/> 
         <span className={`${partnerStyles.learnMore} ${Styles.textShadowPrimary}`}>Learn More</span>
     </Button>
-    <Modal open={open} onclose={handleClose}>
+    {/* <div className={partnerStyles.closeModal} onClick={handleClose}></div> */}
+      <Modal open={open} onClose={handleClose}>
         <div className={partnerStyles.modalContainer}>
           <div className={partnerStyles.modal}>
             <div className={partnerStyles.modalDetails}>
               <StaticImage  src="../images/example.jpg"/> 
-              <Typography variant="h4" component="h2" textAlign="center" className={Styles.textShadowPrimary} sx={{color:"white"}}>ΜΠΟΜΑΤ</Typography>
-              <Typography sx={{ mt: 2, color:"white" }} textAlign="center">
+              <h2  className={`${Styles.textShadowPrimary} ${partnerStyles.title}`}>ΜΠΟΜΑΤ</h2>
+              <h5>
                 McKinsey is a global management consulting firm committed to helping organizations accelerate sustainable and inclusive growth. 
                 We work with clients across the private, public, and social sectors to solve complex problems and create positive change for all their stakeholders. 
                 We combine bold strategies and transformative technologies to help organizations innovate more sustainably, achieve lasting gains in performance, and build workforces that will thrive for this generation and the next. Our firm is designed to operate as one, united by a strong set of values, including a deep commitment to diversity, and making positive social impact through our work and the way we run our firm. Our consultants include medical doctors, engineers, designers, data scientists, business managers, civil servants, entrepreneurs, and research scientists.
                 They join McKinsey for the opportunity to apply their talents to complex, important challenges.
-              </Typography>
+              </h5>
             </div>
-            <div className={partnerStyles.buttonContainer}>
-              <Button variant="contained" style={{backgroundColor:"#F06427"}} href="http://bomat.gr/" className={partnerStyles.button}>WEBSITE</Button>
-              <Button variant="contained" style={{backgroundColor:"#F06427"}} href="http://bomat.gr/" className={partnerStyles.button}>Career Site</Button>
-            </div>
-            
+            <div className={partnerStyles.linkButtons}>
+              <Button variant="contained" style={{backgroundColor:"#F06427"}} href="http://bomat.gr/" >WEBSITE</Button>
+              <Button variant="contained" style={{backgroundColor:"#F06427"}} href="http://bomat.gr/" >Career Site</Button>
+            </div> 
           </div>
         </div>
-    </Modal>   
+    </Modal>       
     </>
  );
 };
