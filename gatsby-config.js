f1c877ff249eea466bd9d68ba09a4700378bfe02
Ioannis Tsagkaropoulos <johntsagas2003@gmail.com>
2025-03-02 21:29:17 +0200

    feat: add script and gatsby.yml for build process and deploy to gh pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/tedxntua-2023",
   siteMetadata: {
     title: `TEDxNTUA 2023`,
     description: `TEDxNTUA 2023 is held this year, on Saturday, May 13th, at Athens Conservatoire. The theme of the event is MNEME. Join us in this journey, full of intriguing scientific talks, engaging performances and multifaceted workshops.`,
@@ -14,11 +15,11 @@ module.exports = {
       options: {
         spaceId: `u7sxm6fbdxv7`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: 'WDneuapWKR9HwpqeGmclxea2b7ZyFIDm-dA4Uy0gmMk',
+        accessToken: "WDneuapWKR9HwpqeGmclxea2b7ZyFIDm-dA4Uy0gmMk",
       },
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
   ],
-}
+};
